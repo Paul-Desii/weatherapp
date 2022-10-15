4cb79d8acc3f08d0431afd1473c56e3e2fcd748f
Paul-Desii <pauldeskidial@gmail.com>
2022-10-15 03:52:30 +0100

    first commit

---
+++ src/components/api.js
@@ -0,0 +1,12 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'a70037f3ebmshb0d1a8f1be5682bp1a81f7jsnbde307e09033',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL = 'https://wft-geo-db.p.rapidapi.com/v1/geo'
+
+export const WEATHER_API_URL = 'https://api.openweathermap.org/data/2.5'
+export const WEATHER_API_KEY = '5c779dd945672d24f624f5dd6f04f8ab'
